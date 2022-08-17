8bf95e249ecd9a5e6bc3d67db03189f0d660ad5d
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-08-17 15:28:54 +0300

    DS/task/font, images, normalize

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'fonts/Montserrat-Regular.ttf'
+import 'index.css';
+import 'normalize.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
