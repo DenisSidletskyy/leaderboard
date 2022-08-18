195ca342ef7dafb60f0cf3e7bb9fea241b372306
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-08-18 08:49:39 +0300

    DS/fix/button index file

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,16 @@
+import Button from "./components/button/Button";
+import {ReactComponent as Edit} from "images/edit.svg";
+
 const App = () => {
 
     return (
         <>
-          app
+            <Button className={'edit'}><Edit/></Button>
+            <Button className={'orangeWhite'}>New day</Button>
+            <Button className={'blueWhite'}>Add new user</Button>
+            <Button className={'orangeBlack'}>Save</Button>
+            <Button className={'navigate'}>{'<<'}</Button>
+            <Button className={'navigate'}>{'>>'}</Button>
         </>
     );
 }
